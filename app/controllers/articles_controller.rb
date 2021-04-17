58f0563e58352fac9293b725ff769709b9564bce
arb1ona <arbiona.shahu@gmail.com>
2021-04-17 20:13:35 +0200

    Add new article from front-end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,5 +7,17 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new
+        @article=Article.new
+    end
+
+    def create 
+        #require the top level key of article and permit title & desc from there 
+        #to be used to create this Article object/instance variable here
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.save
+        redirect_to @article
+    end
 end
 
