5927c2b504d738f110dfecd72090ed61e23480a9
arb1ona <arbiona.shahu@gmail.com>
2021-04-17 23:30:22 +0200

    Add validation error messages and flash messaging on display

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-The front-end: Creating articles
-
-- Route ~ :new, :create
-- Controller#action ~ new create
-  Strong parameters - whitelisting of data (values associated with attributes) that are received through the params hash. During this process for articles you had to 'whitelist' the data coming through for the title and description fields.
-- View ~ Form to create article
+The front-end: Error messages and flash helpers(hash)
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,21 +3,29 @@ class ArticlesController < ApplicationController
 
         @article = Article.find(params[:id])
     end
-#name the @articles variable whatever you want
+        #name the @articles variable whatever you want
     def index
         @articles = Article.all
     end
 
     def new
-        @article=Article.new
+        @article = Article.new 
+        # having this instance variable allows new.html.erb form to be displayed and this line <% if @article.errors.full.. is gonna be empty and let the form be rendered
+        # otherwise @article.errors.full.. would be nil
     end
 
     def create 
         #require the top level key of article and permit title & desc from there 
         #to be used to create this Article object/instance variable here
         @article = Article.new(params.require(:article).permit(:title, :description))
-        @article.save
-        redirect_to @article
+        if  @article.save
+            redirect_to @article
+            flash[:notice] = "Article was successfully created!"
+        #Once the flash helper has the key of 'notice' which has value of 'Article was created successfully' via the create action,
+        #we can use this helper in the views (upon the redirect) to display the message to the user. Go to application.html.erb
+        else
+            render 'new'
+        end
     end
 end
 
