3bfb378c98e7d92dc411f672a1c943a4d5fa0aa1
arb1ona <arbiona.shahu@gmail.com>
2021-04-26 15:38:18 +0200

    Rubocop added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,49 +1,53 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
-    # require "pry"
-    before_action :set_article, only: [:show, :edit, :update, :destroy]
-    
-    def show
-    end
-    def index
-        @articles = Article.all
-    end
-    def new
-        @article = Article.new 
-    end
-    def edit
-    end
-    def create 
-        @article = Article.new(params_article)
-        if  @article.save
-            redirect_to @article
-            # binding.pry
-            flash[:notice] = "Article was successfully created!"
-        else
-            render 'new'
-        end
-    end
-    def update
-        if @article.update(params_article)
-            redirect_to @article
-           flash[:notice] = "Article was edited successfully!"
-        else
-            render 'edit'
-        end
-    end
-    
-    def destroy
-        @article.destroy
-        redirect_to articles_path
-    end
+  # require "pry"
+  before_action :set_article, only: %i[show edit update destroy]
 
-    private
-    
-    def set_article
-        @article = Article.find(params[:id])
-    end
+  def show; end
 
-    def params_article
-        params.require(:article).permit(:title, :description)
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit; end
+
+  def create
+    @article = Article.new(params_article)
+    if @article.save
+      redirect_to @article
+      # binding.pry
+      flash[:notice] = 'Article was successfully created!'
+    else
+      render 'new'
     end
+  end
+
+  def update
+    if @article.update(params_article)
+      redirect_to @article
+      flash[:notice] = 'Article was edited successfully!'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def params_article
+    params.require(:article).permit(:title, :description)
+  end
 end
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
-class User < ApplicationRecord
+# frozen_string_literal: true
 
+class User < ApplicationRecord
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false },
+                       length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 end
