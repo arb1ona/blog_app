f3e4f66f56d191a06f519adf0190da5eab6cfb05
arb1ona <arbiona.shahu@gmail.com>
2021-05-05 01:12:45 +0200

    Show user picture using Gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+    def gravatar_for(user, options = {size: 80})
+        email_address = user.email.downcase
+        hash = Digest::MD5.hexdigest(email_address)
+        size=options[:size]
+        gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+        # we want to return this url in an image tag
+        image_tag(gravatar_url, alt: user.username, class: "rounded-circle shadow mx-auto d-block")
+    end
 end
